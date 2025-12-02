0ff2399b9117c57aee126673be47cbf7ae8066e5
Dylan <rockrz91919@gmail.com>
2025-12-02 13:43:27 -0500

    I finshed the menu initialisation

--- Main.java
+++ Main.java
@@ -27,15 +27,26 @@ public class Main
         
 
         
-        System.out.println("Please select one of the following options");
-        System.out.println("1. Display vending machine opptions");
-        System.out.println("2. Exit");
-        System.out.println();
-        System.out.println("FOR OWNER OF MACHINE");
-        System.out.println("3. Show stock remaining of machine");
-        System.out.println("4. Show total earnings of the machine");
+       
 
-        int choice = a.nextInt();
+        int choice;
+        
+        do{
+             System.out.println();
+             System.out.println("Please select one of the following options");
+             System.out.println("1. Display vending machine opptions");
+             System.out.println("2. Exit");
+             System.out.println();
+             System.out.println("FOR OWNER OF MACHINE");
+             System.out.println("3. Show stock remaining of machine");
+             System.out.println("4. Show total earnings of the machine");
+             choice = a.nextInt();
+             
+             if (choice > 5 || choice < 0) {
+                System.out.println("Invalid choice, please try again");
+             }
+        }
+        while(choice>5 || choice<0);
         switch(choice){
             case 1:
                 vm.displayMenu();
@@ -57,7 +68,7 @@ public class Main
                 System.out.println("Thank you for visiting the machine");
                 break;
                 
-            default: 
+            default: System.out.println("An invalid input has been entered");
                 break;
             
         }
