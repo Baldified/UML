b46143279a344b43d6c8a6ebd58c4155576c09a4
bruce <l4m.bruc3@gmail.com>
2025-12-02 14:00:33 -0500

    merge pls

--- Main.java
+++ Main.java
@@ -22,15 +22,25 @@ public class Main
         
         System.out.println("Welcome to the vending machine");
         
-        System.out.println("Please select one of the following options");
-        System.out.println("1. Display vending machine opptions");
-        System.out.println("4. Exit");
-        System.out.println();
-        System.out.println("FOR OWNER OF MACHINE");
-        System.out.println("2. Show stock remaining of machine");
-        System.out.println("3. Show total earnings of the machine");
+        int choice;
         
-        int choice = a.nextInt();
+        do{
+             System.out.println();
+             System.out.println("Please select one of the following options");
+             System.out.println("1. Display vending machine opptions");
+             System.out.println("2. Exit");
+             System.out.println();
+             System.out.println("FOR OWNER OF MACHINE");
+             System.out.println("3. Show stock remaining of machine");
+             System.out.println("4. Show total earnings of the machine");
+             choice = a.nextInt();
+             
+             if (choice > 5 || choice < 0) {
+                System.out.println("Invalid choice, please try again");
+             }
+        }
+        while(choice>5 || choice<0);
+
         switch(choice){
             case 1:
                 vm.displayMenu();
@@ -52,7 +62,7 @@ public class Main
                 System.out.println("Thank you for visiting the machine");
                 break;
                 
-            default: 
+            default: System.out.println("An invalid input has been entered");
                 break;
             
         }
