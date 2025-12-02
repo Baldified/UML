b2fbd1d259358a9bb8f10c7a9dd0f1192c761a42
Dylan <rockrz91919@gmail.com>
2025-12-02 14:19:12 -0500

    Created test classes

---
+++ VendingMachineTest.java
@@ -0,0 +1,51 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class VendingMachineTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class VendingMachineTest
+{
+    /**
+     * Default constructor for test class VendingMachineTest
+     */
+    public VendingMachineTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void displayMenu()
+    {
+        VendingMachine vendingM1 = new VendingMachine();
+        vendingM1.displayMenu();
+        vendingM1.selectItem(101);
+    }
+}
+
